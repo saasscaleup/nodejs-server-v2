74c36543cab544ce58da6513b6039b12f9f28887
Scale-Up SaaS <100524415+saasscaleup@users.noreply.github.com>
2022-05-11 15:57:00 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1'; // Your server ip address
+const port = 3000;
+
+const version = '1';
+
+const server = http.createServer((req, res) => {
+    
+    const query_object  = url.parse(req.url, true).query;
+    const req_path      = url.parse(req.url, true).pathname;
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    
+    if (req_path == '/') { //check the URL of the current request
+
+        // set response content    
+        res.write(`<html><body><h1 style="color:blue"> [version ${version}]: This is Home Page of Server #2</h1></body></html>`);
+        res.end();
+
+    }else if (req_path == "/health") {
+
+        // set response content
+        res.write(`<html><body><h1 style="color:green"> [version ${version}]: This is Health Check Page of Server #2</h1></body></html>`);
+        res.end();
+
+    }else if (req_path == '/fibo') { // Incresing server CPU!!!
+
+        let num = query_object.num ? query_object.num : 44;
+        // set response content
+        res.write(`<html><body><h1 style="color:yellow">[version ${version}]: Fibo numbers: ${fibo(num)}</h1></body></html>`);
+        res.end();
+
+    }else{
+
+        // set Invalid response content
+        res.statusCode = 401;
+        res.end(`[Version ${version}]: Page Not Found`);
+    }
+
+   console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+});
+
+// Incresing server CPU!!!
+function fibo(n) { 
+    if (n < 2)
+        return 1;
+    else   
+        return fibo(n - 2) + fibo(n - 1);
+}
+
+server.listen(port, '0.0.0.0', () =>{
+    console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
+});
