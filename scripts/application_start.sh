ddb1b13943906cbb4ebc6b7d4494eeb859d7d7f1
Scale-Up SaaS <100524415+saasscaleup@users.noreply.github.com>
2022-06-23 18:37:22 +0300

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo $USER
-
-echo 'sudo su ubutnu'
-sudo su ubuntu
-echo $USER
-
 echo 'cd'
 cd /home/ubuntu/app/nodejs-server-v2
 
-echo 'pm2 restart'
-pm2 restart nodejs-server
+node app.js &
